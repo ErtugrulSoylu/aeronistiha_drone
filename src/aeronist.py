b2d462b9b34f099949b564632c8f8fbdfaf496a7
ErtugrulSoylu <ertugrul.soylu@ogr.iuc.edu.tr>
2022-10-12 01:54:55 +0300

    donus fonksiyonu eklendi. fonksiyonlara descriptionlar eklendi

--- src/aeronist.py
+++ src/aeronist.py
@@ -26,65 +26,83 @@ class drone:
             self.connection.wait_heartbeat()
             print('IHA PIXHAWKA BAGLANDI\n')
 
-    def detail(self, message):
+    def __detail(self, message):
         if self.detailed: print(message)
 
-    def resultMessage(self, command, success=True, failReason="BILINMEYEN BIR HATA"):
+    def __resultMessage(self, command, success=True, failReason="BILINMEYEN BIR HATA"):
         if success:
             print(command, "KOMUTU ICRA EDILDI\n")
         else:
             print(command, "KOMUTU", failReason, "NEDENIYLE ICRA EDILEMEDI\n")
 
     def arm_only(self):
+        """
+            IHA'yi arm eder.
+        """
+
         print("\nARM KOMUTU ALINDI")
 
-        self.detail("\ARM SINYALI GONDERILIYOR")
+        self.__detail("\ARM SINYALI GONDERILIYOR")
         self.connection.mav.command_long_send(self.connection.target_system,
             self.connection.target_component,
             mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-        self.detail("\tARM SINYALI GONDERILDI\n")
+        self.__detail("\tARM SINYALI GONDERILDI\n")
 
         print("\tARAÇ ARM EDILIYOR")
         self.connection.motors_armed_wait()
         print("\tARAÇ ARM EDILDI")
 
-        self.resultMessage("ARM")
+        self.__resultMessage("ARM")
 
-    def arm_and_takeoff(self, aTargetAltitude):
-        print("\nTAKEOFF KOMUTU ALINDI: HEDEF_YÜKSEKLİK=%d Metre" %(aTargetAltitude))
+    def arm_and_takeoff(self, targetAltitude):
+        """
+            targetAltitude: IHA'nin metre cinsinden yukselmesi istenilen yukseklik.
+
+            IHA arm edilir ve istenilen yukseklige ulasmak uzere kalkisa gecer. Yeterince
+            yukselene kadar program bloklanir. 
+        """
+        
+        print("\nTAKEOFF KOMUTU ALINDI: HEDEF_YÜKSEKLİK=%d Metre" %(targetAltitude))
         
         self.arm_only()
 
-        self.detail("\tTAKEOFF SINYALI GONDERILIYOR")
+        self.__detail("\tTAKEOFF SINYALI GONDERILIYOR")
         self.connection.mav.command_long_send(self.connection.target_system,
             self.connection.target_component,
-            mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, aTargetAltitude)
-        self.detail("\tTAKEOFF SINYALI GONDERILDI")
+            mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, targetAltitude)
+        self.__detail("\tTAKEOFF SINYALI GONDERILDI")
 
         print("\tARAÇ YÜKSELİYOR")
         while 1:
             msg = self.connection.recv_match(
                 type='LOCAL_POSITION_NED', blocking=True)
             print('\t\tYUKSEKLIK: %f metre' %(-msg.z))
-            if -msg.z > aTargetAltitude * 0.98:
+            if -msg.z > targetAltitude * 0.98:
                 break
         print('\tHEDEF YÜKSEKLİĞE ULAŞILDI')
 
-        self.resultMessage("TAKEOFF")
+        self.__resultMessage("TAKEOFF")
 
-    def changeVehicleMode(self, mode):
+    def changeVehicleMode(self, mode : str):
+        """
+            mode: Gecis yapilacak mod
+
+            IHA'nin istenilen moda gecirilmesini saglar. Moda gecilene kadar program bloklanir.
+            Eger istenilen mod gecerli degilse hata mesaji dondurur.
+        """
+
         print("\nMOD DEGISTIRME KOMUTU ALINDI: HEDEF MOD=\"%s\"" %(mode))
 
         if mode not in self.connection.mode_mapping():
             print("BILINMEYEN MOD : {}".format(mode))
             print("UYGUN MODLAR : ", list(self.connection.mode_mapping().keys()))
-            self.resultMessage("MOD DEGISTIRME", success=False, failReason="BILINMEYEN MOD")
+            self.__resultMessage("MOD DEGISTIRME", success=False, failReason="BILINMEYEN MOD")
             return
 
         mode_id = self.connection.mode_mapping()[mode]
-        self.detail("\tMOD DEGISTIRME SINYALI GONDERILIYOR")
+        self.__detail("\tMOD DEGISTIRME SINYALI GONDERILIYOR")
         self.connection.set_mode(mode_id)
-        self.detail("\tMOD DEGISTIRME SINYALI GONDERILDI")
+        self.__detail("\tMOD DEGISTIRME SINYALI GONDERILDI")
 
         print("\tMOD DEGISIMI BEKLENIYOR")
         while True:
@@ -94,14 +112,18 @@ class drone:
             if ack_msg["command"] != mavutil.mavlink.MAV_CMD_DO_SET_MODE:
                 continue
 
-            self.detail(mavutil.mavlink.enums["MAV_RESULT"][ack_msg["result"]].description)
+            self.__detail(mavutil.mavlink.enums["MAV_RESULT"][ack_msg["result"]].description)
             print("\tMOD DEGISTIRILDI YENI MOD:\"%s\"" %(mode))
 
             break
 
-        self.resultMessage("MOD DEGISTIRME")
+        self.__resultMessage("MOD DEGISTIRME")
 
     def immadiateLanding(self):
+        """
+            Ani inis modu. IHA inise zorlanir, inis yapilana kadar program bloklanir.        
+        """
+
         print("\nACIL INIS KOMUTU ALINDI")
 
         self.changeVehicleMode("LAND")
@@ -113,22 +135,34 @@ class drone:
             print('\t\tYUKSEKLIK: %f metre' %(-msg.z))
             if -msg.z > 0.1:
                 break
-        print('\tIHA BASARIYLA INDI')
+        print('\tIHA BASARIYLA INIS YAPTI')
         
-        self.resultMessage("ACIL INIS")
+        self.__resultMessage("ACIL INIS")
 
     def move(self, x, y, z, vx=0, vy=0, vz=0, afx=0, afy=0, afz=0, yaw=0, yaw_rate=0, coordinate_frame=mavutil.mavlink.MAV_FRAME_LOCAL_NED, type_mask=int(0b110111111000)):
+        """
+            x, y, z: Gidilecek koordinatlar
+            vx, vy, vz: Hizlar
+            afx, afy, afz: Ivmeler
+            yaw: Donus hizi
+            yaw_rate: Donus frekansi
+            coordinate_frame: Koordinatlarin tipi (global veya yerel koordinatlar gibi)
+
+            Istenilen koordinatlara gidilmesini saglar. Noktaya ulasana kadar program bloklanir.
+        """
+
         print('\nMOVE KOMUTU ALINDI')
         
-        self.detail('MOVE SINYALI GONDERILIYOR')
+        self.__detail('MOVE SINYALI GONDERILIYOR')
         self.connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, self.connection.target_system, self.connection.target_component,
             coordinate_frame, type_mask,
             x, y, z, #konum
             vx, vy, vz, #hiz
             afx, afy, afz, #ivme
             yaw, yaw_rate)) #yaw
-        self.detail('MOVE SINYALI GONDERILDI')
+        self.__detail('MOVE SINYALI GONDERILDI')
 
+        print('\tIHA YOLA CIKIYOR')
         while 1:
             msg = self.connection.recv_match(
                 type='LOCAL_POSITION_NED', blocking=True)
@@ -139,7 +173,43 @@ class drone:
                 print('\tHEDEFE ULASILDI')
                 break
 
-        self.resultMessage('MOVE')
+        self.__resultMessage('MOVE')
+
+    def rotate(self, angle, angular_speed=25, direction=1, relative=1):
+        """
+            angle: Donulmesi istenilen aci degeri
+            angular_speed: Acisal hiz
+            direction: 1: Saat yonu, -1: Saat yonunun tersi
+            relative: 0: Mutlak aci, 1: Bagimli aci
+        """
+        
+        if direction:   print('\nROTATE KOMUTU ALINDI: %d DERECE SAAT YONUNDE DONULUYOR' %(angle))
+        else:           print('\nROTATE KOMUTU ALINDI: %d DERECE SAAT YONUNUN TERSINE DONULUYOR' %(angle))
+        
+        self.__detail('ROTATE SINYALI GONDERILIYOR')
+        self.connection.mav.command_long_send(self.connection.target_system,
+            self.connection.target_component,
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, 0,
+            angle, angular_speed, direction, relative,
+            0, 0, 0)
+        self.__detail('ROTATE SINYALI GONDERILDI')
+
+        print('\tIHA DONUSE BASLIYOR')
+        
+        for i in range(3, 0, -1):
+            print('\t\tGERI SAYIM, %d' %(i))
+            time.sleep(1)
+
+        while 1:
+            msg = self.connection.recv_match(
+                type='ATTITUDE', blocking=True)
+
+            print('\t\tDONUS HIZI: %f' %(msg.yawspeed))
+            if abs(msg.yawspeed) < 0.01:
+                print('\tDONUS TAMAMLANDI')
+                break
+
+        self.__resultMessage('ROTATE')
 
     # def tespit(self, model, path):
     #     if os.path.isfile(path) is False:
--- src/test.py
+++
@@ -1,6 +0,0 @@
-from aeronist import drone
-
-vehicle = drone(simulation=True)
-vehicle.changeVehicleMode('GUIDED')
-vehicle.arm_and_takeoff(5)
-vehicle.move(5, 10, -4)
